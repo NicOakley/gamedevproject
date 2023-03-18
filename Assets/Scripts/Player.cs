840200a533dc2668468095d66bd3d67d888eb347
NicOakley <personal@nicoakley.com>
2023-03-18 03:59:36 -0300

    Merge fix for player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 public class Player : MonoBehaviour
 {
-
     public InventoryManager inventoryManager;
+    public Item item;
 
     public static int gold = 0;
+    public float health = 100f;
+    public float atkDamage = 20f;
+    private HealthBar HealthBar;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        HealthBar = GetComponentInChildren<HealthBar>();
+        HealthBar.MAX_HEALTH = health;
+        HealthBar.health = health;
     }
 
     // Update is called once per frame
